8209212687db83abd17b2d8e49e7c40c4925eb06
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-11-05 23:20:26 +0000

    function to change background

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let btnsubmit = document.getElementById('btnsubmit');
 //regex to verify email
 let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
 
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth === 375) {
-        body.style.backgroundImage= "url\(\"images/bg-curvy-mobile.svg\")";
-        body.style.backgroundPositionY= '33em';
+        body.style.backgroundImage = "url\(\"images/bg-curvy-mobile.svg\")";
+        body.style.backgroundPositionY = '33em';
     }
 })
