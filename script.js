8dec59971a3ee5ca0a2a010a57daf8d7eabbc6ba
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-11-06 23:08:22 +0000

    responsive curvy bg

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ let btnsubmit = document.getElementById('btnsubmit');
 //regex to verify email
 let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
 
-if (window.innerWidth === 375) {
+
+if (window.innerWidth <= 568) {
     body.style.backgroundImage = "url\(\"images/bg-curvy-mobile.svg\")";
-    body.style.backgroundPositionY = '31em';
-}else if (window.innerWidth === 1440) {
+    body.style.backgroundPositionY = '11%';
+    
+}else if (window.innerWidth <= 768) {
     body.style.backgroundImage = "url\(\"images/bg-curvy-desktop.svg\")";
-    body.style.backgroundPositionY = '34em';
+    body.style.backgroundPositionY = '13%';
+
+}else if (window.innerWidth <= 1440) {
+    body.style.backgroundImage = "url\(\"images/bg-curvy-desktop.svg\")";
+    body.style.backgroundPositionY = '19%';
+
 }
 
 btnsubmit.addEventListener('click', () => {
