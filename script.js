8536dfc25193c078b8763edc7a95918e4605f558
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-11-06 03:34:40 +0000

    modified backround screens 375px or 1440px

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let btnsubmit = document.getElementById('btnsubmit');
 //regex to verify email
 let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
 
-window.addEventListener('DOMContentLoaded', () => {
 if (window.innerWidth === 375) {
     body.style.backgroundImage = "url\(\"images/bg-curvy-mobile.svg\")";
     body.style.backgroundPositionY = '31em';
@@ -14,7 +13,6 @@ if (window.innerWidth === 375) {
     body.style.backgroundImage = "url\(\"images/bg-curvy-desktop.svg\")";
     body.style.backgroundPositionY = '34em';
 }
-})
 
 btnsubmit.addEventListener('click', () => {
     if (regex.test(inputemail.value) === false) {
